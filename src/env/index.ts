90a7139bbe4ba3545b32e03e94c6d140cca1662a
Lucas <loisabel@rd.com.br>
2024-10-28 01:04:44 +0000

    feat: added new settings into project

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error('❌ Invalid enviroment variables', _env.error.format());
+
+  throw new Error('Invalid enviroment variables');
+}
+
+export const env = _env;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { app } from './app';
+import { app } from '@/app';
+import { env } from '@/env';
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3000,
+    port: env.data.PORT,
   })
   .then(() => {
-    console.log('🚀 Server is running at http://localhost:3000');
+    console.log(`🚀 Server is running at http://localhost:${env.data.PORT}`);
   });
