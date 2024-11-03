711f474644304349b6c679fd1f726468fb695e03
Lucas <loisabel@rd.com.br>
2024-11-03 23:15:16 +0000

    feat: added new controller and docker file

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import { fastify } from 'fastify';
+import { z } from 'zod';
+import { prisma } from './lib/prisma';
 
 export const app = fastify();
+
+app.post('/user', async (request, response) => {
+  const registerBodySchema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwrod_hash: password,
+    },
+  });
+
+  response.status(201).send();
+});
--- src/env/index.ts
+++ src/env/index.ts
@@ -6,13 +6,13 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
+  DATABASE_URL: z.string().default(''),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
   console.error('❌ Invalid enviroment variables', _env.error.format());
-
   throw new Error('Invalid enviroment variables');
 }
 
