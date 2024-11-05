9349abf78b359ae6231d1e29106c36a8f8eaf4f6
Lucas <loisabel@rd.com.br>
2024-11-05 20:30:45 +0000

    feat: implemented first route

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,5 @@
 import { fastify } from 'fastify';
-import { z } from 'zod';
-import { prisma } from './lib/prisma';
+import { appRoutes } from './http/routes';
 
 export const app = fastify();
-
-app.post('/user', async (request, response) => {
-  const registerBodySchema = z.object({
-    name: z.string().min(3),
-    email: z.string().email(),
-    password: z.string().min(6),
-  });
-
-  const { name, email, password } = registerBodySchema.parse(request.body);
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      passwrod_hash: password,
-    },
-  });
-
-  response.status(201).send();
-});
+app.register(appRoutes);
